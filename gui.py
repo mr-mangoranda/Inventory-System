4dd1450a94c6ef6e7a92cbef4cffdc1de5f10886
mr-mangoranda <habibmangoranda15@gmail.com>
2025-04-09 10:13:14 -0800

    Update Item (Button)

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
-from inventory import load_data, add_item
+from inventory import load_data, add_item, update_item
 
 # ----- Function ----- #
 
@@ -31,7 +31,7 @@ def open_add_item_window():
     add_window = tk.Toplevel(root)
     add_window.title("Add New Item")
 
-    tk.label(add_window, text="Item Name:").pack()
+    tk.Label(add_window, text="Item Name:").pack()
     name_entry = tk.Entry(add_window)
     name_entry.pack()
 
@@ -44,6 +44,42 @@ def open_add_item_window():
     price_entry.pack()
 
     tk.Button(add_window, text="Add Item", command=submit).pack(pady=5)
+
+def open_update_item_window():
+    selected = inventory_list.curselection()
+    if not selected:
+        messagebox.showerror("Error", "Please select an item to update.")
+        return
+    
+    item_text = inventory_list.get(selected[0])
+    item_name = item_text.split(" - ")[0] #extract name from the text
+
+    def submit_update():
+        try:
+            new_quantity = int(quantity_entry.get())
+            new_price = float(price_entry.get())
+            update_item(item_name, new_quantity, new_price)
+            messagebox.showinfo("Success", f"{item_name} update successfully")
+            update_window.destroy()
+            view_inventory_gui()
+
+        except ValueError:
+            messagebox.showerror("Error", "Please enter valid quantity and price!")
+
+    update_window = tk.Toplevel(root)
+    update_window.title(f"Update Item - {item_name}")
+
+    tk.Label(update_window, text=f"Update {item_name}").pack(pady=5)
+
+    tk.Label(update_window, text="New Quantity:").pack()
+    quantity_entry = tk.Entry(update_window)
+    quantity_entry.packk()
+
+    tk.Label(update_window, text="New Price:").pack()
+    price_entry = tk.Entry(update_window)
+    price_entry.pack()
+
+    tk.Button(update_window, text="Update Item", command=submit_update).pack(pady=5)
 
 # ----- GUI Setup ----- #
 
@@ -70,6 +106,10 @@ inventory_list.pack(pady=10)
 
 add_button = tk.Button(frame, text="Add Item", command=open_add_item_window)
 add_button.pack(pady=5)
+
+# ----- Update Item ----- #
+
+update_button= tk.Button(frame, text="Update Item", command=open_update_item_window )
 
 # ----- Start GUI loop ----- #
 root.mainloop()
